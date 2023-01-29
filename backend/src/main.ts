34bd489c4dba42a0ef634c09be89c43dd5db1b39
Higa <yh0331tigers@yahoo.co.jp>
2023-01-29 18:55:21 +0900

    cookie-parser修正

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { Request } from 'express';
-import * as cookieParser from 'Cookie-Parser';
+import * as cookieParser from 'cookie-parser';
 import * as csurf from 'csurf';
 
 async function bootstrap() {
