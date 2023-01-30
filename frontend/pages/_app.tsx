f6dfdb6897970a3705e1694b571c3da01f8fc358
Higa <yh0331tigers@yahoo.co.jp>
2023-01-30 21:40:00 +0900

    tailwind等のセットアップ、ロード時のcsrf token付与を実装

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,6 +1,38 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import axios from "axios";
+import { useEffect } from "react";
+import { QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      refetchOnWindowFocus: false
+    },
+  },
+});
+
+function MyApp({Component, pageProps }: AppProps) {
+  //front back間でcookieのやりとりを行うための設定
+  axios.defaults.withCredentials = true;
+  //アプリケーションがロードした時にcsrf tokenを取得
+  useEffect(() => {
+    const getCsrfToken = async () => {
+      const { data } = await axios.get(
+          `${process.env.NEXT_PUBLIC_API_URL}/auth/csrf`
+      )
+      axios.defaults.headers.common['csrf-token'] = data.csrfToken
+    }
+    getCsrfToken();
+  }, [])
+  return (
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
+  );
 }
+
+export default MyApp;
---
+++ frontend/types/index.ts
@@ -0,0 +1,42 @@
+import React from "react";
+
+export type AuthForm = {
+    email: string,
+    password: string,
+}
+export type EditedPost = {
+    id: string,
+    title: string,
+    //name => category name
+    name?: string | null,
+    desc?: string | null,
+    status: string,
+}
+export type PostType = {
+    id: string,
+    title: string,
+    desc: string,
+    likes: number,
+    status: string,
+    image: string,
+    createdAt: number,
+    updatedAt: number,
+    userId: string,
+    categories?: Array<CategoryButtonType>,
+    comments: string[],
+}
+export type CategoryProps = {
+    onClick?: (event: React.MouseEvent<HTMLInputElement>) => void;
+    categories?: Array<CategoryButtonType>;
+}
+export type CategoryButtonType = {
+    category: {
+        name: string[]
+    }
+}
+export type CommentType = {
+
+}
+export type EditedComment = {
+
+}
