4d87499b54880626b843d33a75aa9befa260a19f
Higa <yh0331tigers@yahoo.co.jp>
2023-02-07 19:17:12 +0900

    blog記事のカテゴリを,や空白で複数登録できるように修正

--- backend/src/category/category.service.ts
+++ backend/src/category/category.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
-import { CategoryDto } from './dto/category.dto';
 import { Category } from '@prisma/client';
 import { CreatePostDto } from '../post/dto/create-post.dto';
 
@@ -10,7 +9,9 @@ export class CategoryService {
   //カテゴリーの作成
   async createCategory(userId: string, dto: CreatePostDto): Promise<string[]> {
     //カテゴリを配列に変換して、重複を削除する
-    const splitCategory = Array.from(new Set(dto.name.split(', ')));
+    const splitCategory = Array.from(new Set(dto.name.split(/[,\s]+/))).filter(
+      (s) => s.trim() !== '',
+    );
     const categoryArg = [];
     for (let i = 0; i < splitCategory.length; i++) {
       const postCategory = await this.prisma.category.findFirst({
