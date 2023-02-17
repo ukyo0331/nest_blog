e71bb3da3f6a2c5076eadd68d28444c7f13f97fd
Higa <yh0331tigers@yahoo.co.jp>
2023-02-17 23:36:28 +0900

    不要なパッケージ削除、ヘッドラインリストスタイル修正

--- frontend/src/components/ArticleHeadingListLayout.tsx
+++ frontend/src/components/ArticleHeadingListLayout.tsx
@@ -2,57 +2,58 @@ import { useRouter } from "next/router";
 import { format } from "timeago.js";
 import CategoryButton from "./CategoryImageButton";
 import { PostType } from "../../types";
-import { useState } from 'react';
 
 type ArticleHeadingListLayoutType = {
   recentPostData: Array<PostType>
 }
+
 //記事見出しのレイアウト
 const ArticleHeadingListLayout = ({recentPostData}: ArticleHeadingListLayoutType) => {
   const router = useRouter();
-  const [clicked, setClicked] = useState(false);
-  const handleClick = () => {
-    setClicked(true);
-    setTimeout(() => {
-      setClicked(false);
-    }, 500);
-  };
-  return (
-    <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4'>
-      {recentPostData?.map((headline: PostType) => {
-        const { id, title, categories, likes, createdAt } = headline;
 
-        return (
-          <a key={id}
-             onClick={(e) => {
-               e.preventDefault();
-               router.push(`/blog/${id}`)
-             }
-             }>
-            <div className='rounded-lg shadow-lg h-36 cursor-pointer break-all relative m-3'>
-              <div>
-                <div className='overflow-scroll hidden-scrollbar ml-2 mt-2'>
-                  <CategoryButton
-                    categories={categories}
-                  />
-                </div>
-                <p className='text-4xl m-3'>
-                  {title}
-                </p>
-                <div className='flex'>
-                  <div className='absolute bottom-1 left-1'>
-                    ♡{likes}
+  return (
+    <section className='pt-[100px] pl-0 pr-0'>
+      <div className='m-auto'>
+        <h2 className='min-h-0 font-light text-[clamp(40px,5.2vw,70px)] ml-8'>
+          Recent Posts
+          <span className='block text-lg text-[#666]'>最近の投稿</span>
+        </h2>
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 m-3'>
+          {recentPostData?.map((headline: PostType) => {
+            const { id, title, categories, likes, createdAt } = headline;
+            return (
+              <a key={id}
+                 onClick={(e) => {
+                   e.preventDefault();
+                   router.push(`/blog/${id}`)
+                 }
+                 }>
+                <div className='rounded-lg shadow-lg h-48 cursor-pointer break-all relative m-3'>
+                  <div className='h-full flex flex-col'>
+                    <div className='overflow-scroll hidden-scrollbar ml-2 mt-2'>
+                      <CategoryButton
+                        categories={categories}
+                      />
+                    </div>
+                    <p className='text-2xl ml-3 mt-1 flex-1 overflow-hidden overflow-ellipsis'>
+                      {title}
+                    </p>
+                    <div className='flex'>
+                      <div className='absolute bottom-1 left-1'>
+                        ♡{likes}
+                      </div>
+                      <small className='absolute bottom-1 right-2'>
+                        {format(createdAt)}
+                      </small>
+                    </div>
                   </div>
-                  <div className='absolute bottom-1 right-2'>
-                    {format(createdAt)}
-                  </div>
                 </div>
-              </div>
-            </div>
-          </a>
-        )
-      })}
-    </div>
+              </a>
+            )
+          })}
+        </div>
+      </div>
+    </section>
   )
 };
 
