8a820f965de0a888c136218f065f6216e1baa939
Higa <yh0331tigers@yahoo.co.jp>
2023-03-24 21:48:15 +0900

    table of contentsの機能概形完成

--- frontend/src/components/Toc.tsx
+++ frontend/src/components/Toc.tsx
@@ -1,44 +1,53 @@
 import { useEffect, useRef, useState } from 'react';
 import TocItem from './TocItem';
+import { useRouter } from 'next/router';
 
 const Toc = () => {
+  const router = useRouter();
   const headingRef = useRef<Element[]>();
   const scrollRef = useRef(0);
   const [active, setActive] = useState('');
   useEffect(() => {
-    const headings = Array.from(
-      document.querySelectorAll('h2[id], h3[id]')
-    );
-    const ids = headings.map((e) => e.id);
-    headingRef.current = headings;
-    const observer = new IntersectionObserver(
-      (entries) => {
-        entries.forEach((entry) => {
-          const id = entry.target.getAttribute('id') ?? '';
-          if (entry.isIntersecting) { //entry: 監視対象の要素　が入った状態
-            setActive(id);
-            scrollRef.current = window.scrollY;
-            return;
-          }
-          const diff = scrollRef.current - window.scrollY;
-          const isScrollingUp = diff > 0;
-          const currentIndex = ids.indexOf(id);
-          const prevEntry = ids[currentIndex - 1];
-          if (isScrollingUp) {
-            setActive(prevEntry);
-          }
-        })
-      },
-      {
-        rootMargin: '0% 0% -85% 0%',
-        threshold: 0.2,
-      }
-    );
-    headings.forEach((element) => observer.observe(element));
-    return () => {
-      headings.forEach((element) => observer.unobserve(element));
-    };
-  }, []);
+    const highlightToc = () => {
+      const headings = Array.from(
+        document.querySelectorAll('h2[id], h3[id]')
+      );
+      const ids = headings.map((e) => e.id);
+      headingRef.current = headings;
+      const observer = new IntersectionObserver(
+        (entries) => {
+          entries.forEach((entry) => {
+            const id = entry.target.getAttribute('id') ?? '';
+            if (entry.isIntersecting) { //entry.isIntersecting: 監視対象の要素　が監視枠に入った状態
+              setActive(id);
+              scrollRef.current = window.scrollY;
+              return;
+            }
+            const diff = scrollRef.current - window.scrollY;
+            const isScrollingUp = diff > 0; //上スクロールしたらtrue
+            const currentIndex = ids.indexOf(id);
+            const prevEntry = ids[currentIndex - 1];
+            if (isScrollingUp) {
+              setActive(prevEntry);
+            }
+          })
+        },
+        {
+          rootMargin: '0% 0% -85% 0%',
+          threshold: 0.2,
+        }
+      );
+      headings.forEach((element) => observer.observe(element));
+      return () => {
+        headings.forEach((element) => observer.unobserve(element));
+      };
+    }
+    router.events.on('routeChangeComplete', () => {
+      highlightToc();
+    });
+
+    highlightToc();
+  }, [headingRef.current]);
 
   const handleTocItemClick = (id: string) => {
     setActive(id);
